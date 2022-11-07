320a0b42ddb4fe5478d3ea2311d934565813a986
Alex <37459473+gh0st91@users.noreply.github.com>
2022-11-07 15:26:56 -0800

    Update rfid_brute_dict_generator.py

--- rfid_brute_dict_generator.py
+++ rfid_brute_dict_generator.py
@@ -1,7 +1,8 @@
 import sys
 
-filename = input("Enter the name of the file to save to: ") + ".txt" # Name of the file and append .txt
+filename = input("Enter the name of the file to save to: ") + ".txt"
 
-with open(filename, 'w') as sys.stdout: # Open a file (brute.txt) to write to.
-	for i in range(16777216): # The range of possible 48-bit combinations.
-		print(hex(i)[2:].zfill(6).upper()) # Change the zfill number to change the number of hex chars.
+with open(filename, 'w') as sys.stdout: 	   # Open a file (brute.txt) to write to.
+	for i in range(16777216): 		   # The range of possible 48-bit combinations.
+		print(hex(i)[2:].zfill(6).upper()) # Change the zfill number to change the number of hex chars. 
+						   # If you do, make sure to change the range accordingly!
