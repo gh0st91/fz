516a5464e529c8f88dd9d128aa47ffe3d6209d9a
Alex <37459473+gh0st91@users.noreply.github.com>
2022-11-07 15:29:19 -0800

    Update rfid_brute_dict_generator.py

--- rfid_brute_dict_generator.py
+++ rfid_brute_dict_generator.py
@@ -2,7 +2,7 @@ import sys
 
 filename = input("Enter the name of the file to save to: ") + ".txt"
 
-with open(filename, 'w') as sys.stdout:		  # Open a file (brute.txt) to write to.
+with open(filename, 'w') as sys.stdout:		  # Open a file (user input) to write to.
 	for i in range(16777216):			  # The range of possible 48-bit combinations.
 		print(hex(i)[2:].zfill(6).upper()) # Change the zfill number to change the number of hex chars. 
 							  # If you do, make sure to change the range accordingly!
