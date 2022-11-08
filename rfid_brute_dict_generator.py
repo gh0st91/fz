b287d31c970b356f796e8695b0b3208d1d7c716c
Alex <37459473+gh0st91@users.noreply.github.com>
2022-11-08 12:10:32 -0800

    Corrected comment regarding range of bits
    Changed from 48-bit to 26-bit

--- rfid_brute_dict_generator.py
+++ rfid_brute_dict_generator.py
@@ -3,6 +3,6 @@ import sys
 filename = input("Enter the name of the file to save to: ") + ".txt"
 
 with open(filename, 'w') as sys.stdout:		  # Open a file with the name supplied by the user.
-	for i in range(16777216):			  # The range of possible 48-bit combinations.
+	for i in range(16777216):			  # The range of possible 26-bit combinations.
 		print(hex(i)[2:].zfill(6).upper()) # Change the zfill number to change the number of hex chars. 
 							  # If you do, make sure to change the range accordingly!
